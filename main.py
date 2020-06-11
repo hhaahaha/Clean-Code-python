de9cc9b779763dfedd3d520d26adc3769b78723f
JinJoo Lee <49986339+hhaahaha@users.noreply.github.com>
2020-06-11 21:53:27 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -3,52 +3,67 @@ import torch
 
 import numpy as np
 import random
+import argparse
 import time
 import datetime
+import os
 
 from model import dataloader, module
 
+# def device_settings():
+# GPU 디바이스 이름 구함
+# device_name = tf.test.gpu_device_name()
+device_name = torch.cuda.get_device_name()
+
+# GPU 디바이스 이름 검사
+if device_name == '/device:GPU:0':
+    print('Found GPU at: {}'.format(device_name))
+else:
+    raise SystemError('GPU device not found')
+
+# 디바이스 설정
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def train(opt):
+    """  """
+
 
 if __name__ == "__main__":
-    # Configuration Parameters
-    train_path = "CleanCode_Bert/myCleanCode/nsmc/ratings_train.txt"
-    test_path = "CleanCode_Bert/myCleanCode/nsmc/ratings_test.txt"
-    doc_column = 'document'
-    label_column = 'label'
-    model_name = 'multilingual-cased'
-    # 배치 사이즈
-    batch_size = 32
-    # 입력 토큰의 최대 시퀀스 길이
-    MAX_LEN = 128
+    # # Configuration Parameters
 
-    # default value
-    seed_val = 1234
+    # # 입력 토큰의 최대 시퀀스 길이
+    # MAX_LEN = 128
 
-    # 에폭수
-    epochs = 4
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--experiment_name', help='Where to store logs and models')
+    parser.add_argument('--train_data',required=True, help='path to training dataset')
+    parser.add_argument('--test_data',required=True, help='path to test dataset')
+    parser.add_argument('--manualSeed',type=int, defalut=1234, help='for random seed setting')
+    parser.add_argument('--batch_size', type=int, default=32, help='input batch size')
+    parser.add_argument('--epoch', type=int, default=4, help='input epoch num')
+    parser.add_argument('--saved_model', default='', help='path to model to continue training')
 
-    # for inference
-    # model, config, vocab
-    model_path = './model/pt_nsmc_bert_module.pth'
+    opt = parser.parse_args()
 
-    # def device_settings():
-    # GPU 디바이스 이름 구함
-    device_name = tf.test.gpu_device_name()
+    # model_path = './model/pt_nsmc_bert_module.pth'
 
-    # GPU 디바이스 이름 검사
-    if device_name == '/device:GPU:0':
-        print('Found GPU at: {}'.format(device_name))
-    else:
-        raise SystemError('GPU device not found')
+    if not opt.experiment_name:
+        model_name = opt.saved_model.split('/')[-1].split('.')[0]
+        opt.experiment_name = f'{model_name}'
+        opt.experiment_name += f'-Seed{opt.manualSeed}'
 
-    # 디바이스 설정
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-        print('There are %d GPU(s) available.' % torch.cuda.device_count())
-        print('We will use the GPU:', torch.cuda.get_device_name(0))
-    else:
-        device = torch.device("cpu")
-        print('No GPU available, using the CPU instead.')
+    os.makedirs(f'./saved_models/{opt.experiment_name}', exist_ok=True)
+
+    """ Seed and GPU setting """
+    random.seed(opt.manualSeed)
+    np.random.seed(opt.manualSeed)
+    torch.manual_seed(opt.manualSeed)
+    torch.cuda.manual_seed(opt.manualSeed)
+
+    train(opt)
+
+
 
     # 재현을 위해 랜덤시드 고정
     random.seed(seed_val)
